542311da0bef7c958d661f0884ea8df7e69c111e
= <andre@bwcenter.com.br>
2020-02-27 14:05:35 -0300

    Novos testes e seprando suites pelo XML do PHP Unit

--- tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -30,22 +30,77 @@ class LeilaoDaoTest extends TestCase
         self::$pdo->beginTransaction();
     }
 
-    public function testInsercaoEBuscaDevemFuncionar()
+    /**
+     * @dataProvider leiloes
+     */
+    public function testeBuscaLeiloesNaoFinalizados(array $leiloes)
     {
-        $leilaoVariant = new Leilao('Variant 1972');
-        
         $leilaoDao = new LeilaoDao(self::$pdo);
-        $leilaoDao->salva($leilaoVariant);
 
+        foreach ($leiloes as $leilao) {
+            $leilaoDao->salva($leilao);
+        }
+        
         $leiloes = $leilaoDao->recuperarNaoFinalizados();
 
         self::assertCount(1, $leiloes);
         self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
         self::assertSame('Variant 1972', $leiloes[0]->recuperarDescricao());
+        self::assertFalse($leiloes[0]->estaFinalizado());
+    }
+
+    /**
+     * @dataProvider leiloes
+     */
+    public function testeBuscaLeiloesFinalizados(array $leiloes)
+    {
+        $leilaoDao = new LeilaoDao(self::$pdo);
+
+        foreach ($leiloes as $leilao) {
+            $leilaoDao->salva($leilao);
+        }
+
+        $leiloes = $leilaoDao->recuperarFinalizados();
+
+        self::assertCount(1, $leiloes);
+        self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
+        self::assertSame('Fiat 147', $leiloes[0]->recuperarDescricao());
+        self::assertTrue($leiloes[0]->estaFinalizado());
+    }
+
+    public function testAoAtualizarLeilaoStatusDeveSerAlterado()
+    {
+        $leilao = new Leilao('Brasilia Amarela');
+        
+        $leilaoDao = new LeilaoDao(self::$pdo);
+        $leilao = $leilaoDao->salva($leilao);
+
+        $leilao->finaliza();
+        $leilaoDao->atualiza($leilao);
+
+        $leiloes = $leilaoDao->recuperarFinalizados();
+
+        self::assertCount(1, $leiloes);
+        self::assertSame('Brasilia Amarela', $leiloes[0]->recuperarDescricao());
+        self::assertTrue($leiloes[0]->estaFinalizado());
     }
 
     public function tearDown(): void
     {
         self::$pdo->rollBack();
+    }
+
+    public function leiloes()
+    {
+        $leilaoNaoFinalizado = new Leilao('Variant 1972');
+
+        $leilaoFinalizado = new Leilao('Fiat 147');
+        $leilaoFinalizado->finaliza();
+
+        return [
+            [
+                [$leilaoNaoFinalizado, $leilaoFinalizado]
+            ]
+        ];
     }
 }
